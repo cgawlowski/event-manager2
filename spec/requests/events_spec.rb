9282e3be7c4dc9e0ad946298d58525f410396f0c
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-09-10 17:05:50 -0400

    Created v1 folder and moved controller in it

---
+++ spec/requests/events_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Events API', type: :request do
+  it 'returns all events' do
+    get '/api/v1/events'
+    
+    expect(response).to have_http_status(:success)
+  end
+end
