e6d631ea3f970a0de376d522a6f91b94e744c886
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-09-19 22:02:28 -0400

    Added first test

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -1,11 +1,5 @@
 require "test_helper"
 
 class EventsTest < ActionDispatch::IntegrationTest
-  describe 'GET events' do
 
-    it 'responds with 200 OK' do
-      get '/api/events'
-      response.status.must_equal 200
-    end
-  end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save event without name" do
+    event = Event.new
+    assert_not event.save
+  end
 end
