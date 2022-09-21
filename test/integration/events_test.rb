2ef87912eebbc136a4d825c6fd6b2dd58df5ce92
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-09-20 22:19:43 -0400

    Added back fixtures

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -3,9 +3,20 @@ require "test_helper"
 class EventsTest < ActionDispatch::IntegrationTest
   describe 'GET events' do
 
+
+    #Test to make sure it renders JSON
     it 'responds with 200 OK' do
       get '/api/events'
       response.status.must_equal 200
     end
+
+    #Testing the content
+    it 'returns all posts' do
+      get '/api/events'
+      json = JSON.parse(response.body)
+      json.size.must_equal 2
+      json[0]['name'].must_equal 'Festival de Jazz de Montréal'
+    end
+
   end
 end
