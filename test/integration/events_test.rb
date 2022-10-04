defc84bbed55c8e5c0980a1f71b2f10e73fc873b
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-10-04 19:47:44 -0400

    Unit tests

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -7,27 +7,26 @@ class EventsTest < ActionDispatch::IntegrationTest
     assert event.valid?
   end
   
-  #Unit tests
+  #Unit testing of the Event model
   test 'events are correctly created' do
     FactoryBot.create :event
   end
 
-  test "requires a name" do
+  test "a name is created" do
     event = FactoryBot.build :event
     event.name = ""
     refute event.valid?
   end
 
-  test "characters limit of 34" do
-    event = FactoryBot.build :event
-    event.description = ""
-    assert event.valid?
+  test "should not save event without a name" do
+    event = Event.new
+    assert !event.save, "Saved the event without a title"
   end
 
-  test "requires a description" do
+  test "description is created" do
     event = FactoryBot.build :event
     event.description = ""
-    assert event.valid?
+    assert event.valid?, "Description has not been added"
   end
 
 end
