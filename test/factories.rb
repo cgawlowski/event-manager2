de681e899e4c47f2a4fb38ad1b06856cd0f03cc0
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-10-04 20:27:24 -0400

    New event testing added

--- test/factories.rb
+++ test/factories.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :event do |f|
       # default attribute values defined here
       name { "Best event ever" }
-      description { "No really this is the best event I've ever read!" }
+      description { "No really this is the best event I've ever been to!" }
       start_date { Time.zone.now+1.day }
       end_date { Time.zone.now+2.day }
   end
--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -29,4 +29,17 @@ class EventsTest < ActionDispatch::IntegrationTest
     assert event.valid?, "Description has not been added"
   end
 
+  # test "user can create a new event" do
+  #   visit root_path
+    
+  #   click_on "New Event"
+
+  #   fill_in "name", with: "Event 1"
+  #   fill_in "description", with: "Description of event 1"
+
+  #   click_button "Create Event"
+
+  #   assert_text "Event created", "Event has not been created"
+  # end
+
 end
