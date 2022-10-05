175ac9c575d1d9fdb11b8171b5dc7f40e1ce3645
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-10-04 22:06:21 -0400

    First system test

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -2,4 +2,9 @@ require "application_system_test_case"
 
 class EventsTest < ApplicationSystemTestCase
 
+  # test "it loads the website name" do
+  #   visit root_path
+  #   assert_text "Event Manager"
+  # end
+
 end
